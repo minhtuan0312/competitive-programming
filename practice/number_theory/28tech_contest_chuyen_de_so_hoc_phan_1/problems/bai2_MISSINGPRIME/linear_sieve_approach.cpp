542bb94011ba8fee36c15057223e2912a36a6735
minhtuan0312 <70638910+minhtuan0312@users.noreply.github.com>
2025-11-02 15:14:45 +0700

    [HSG_Số Học]. Bài 2. Số nguyên tố bị thiếu (MISSINGPRIME)

---
+++ practice/number_theory/28tech_contest_chuyen_de_so_hoc_phan_1/problems/bai2_MISSINGPRIME/linear_sieve_approach.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define all(x) x.begin(), x.end()
+#define FOR(i, a, n) for (int i = a; i < n; i++)
+#define FORd(i, a, n) for (int i = n - 1; i >= a; i--)
+#define minhtuan0312 ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define sz(x) size(x)
+#define pb push_back
+#define fi first
+#define se second
+
+const ll mod = 1e9 + 7;
+
+template<class T>
+void maximize(T &x, const T &y) {
+    if (x < y) x = y;
+}
+
+template<class T>
+void minimize(T &x, const T &y) {
+    if (x > y) x = y;
+}
+
+template<typename T1, typename T2>
+void __print(const pair<T1, T2> &p);
+
+template<typename T, typename... V>
+void __print(const vector<T, V...> &v);
+
+template<typename T1, typename T2, typename... V>
+void __print(const map<T1, T2, V...> &m);
+
+template<typename T>
+void __print(const T &x) {cerr << x;}
+
+template<typename T1, typename T2>
+void __print(const pair<T1, T2> &p) {
+    cerr << '(';
+    __print(p.fi);
+    cerr << ", ";
+    __print(p.se);
+    cerr << ')';
+}
+
+template<typename T, typename... V>
+void __print(const vector<T, V...> &v) {
+    cerr << '[';
+    FOR(i, 0, sz(v)) {
+        if(i) cerr << ", ";
+        __print(v[i]);
+    }
+    cerr << ']';
+}
+
+template<typename T1, typename T2, typename... V>
+void __print(const map<T1, T2, V...> &m) {
+    cerr << '{';
+    bool first = 1;
+    for(const auto &x: m) {
+        if(!first) cerr << ", ";
+        first = 0;
+        __print(x);
+    }
+    cerr << '}';
+}
+
+template<typename T>
+void _print(T t) { __print(t);}
+void _print() {}
+template<typename T, typename... V>
+void _print(T t, V... v) {__print(t); if(sizeof...(v)) cerr << ", "; _print(v...); }
+
+#ifdef LOCAL
+#define deb(...) do {\
+    cerr << "[In " <<  __func__ << "(): line " << __LINE__ << "] [" << #__VA_ARGS__ << "] = [";\
+    _print(__VA_ARGS__);\
+    cerr << ']' << nl;\
+} while(0);
+#else
+#define deb(...)
+#endif
+
+const int limN = 1e6 + 5;
+int lp[limN];
+bool mark[limN];
+vector<int> primes;
+void linear_sieve(void) {
+    for(int i = 2; i < limN; i++) {
+        if(lp[i] == 0) {
+            lp[i] = i;
+            primes.pb(i);
+        }
+        for(int j = 0; j < sz(primes) && i * primes[j] <= limN; j++) {
+            lp[i*primes[j]] = primes[j];
+            if (primes[j] == lp[i]) break;
+        }
+    }
+}
+
+int main(void) {
+    minhtuan0312;
+
+    #define TASK ""
+    if (fopen(TASK ".inp", "r")) {
+        freopen(TASK ".inp", "r", stdin);
+        freopen(TASK ".out", "w", stdout);
+    }
+
+    linear_sieve();
+
+    int n; cin >> n;
+    int A[n];
+    int maxi = 0;
+    FOR(i, 0, n) {
+        cin >> A[i];
+        mark[A[i]] = 1;
+        maximize(maxi, A[i]);
+    }
+
+    for(const auto &p: primes) {
+        if(p > maxi) break;
+        if(!mark[p]) return cout << p, 0;
+    }
+    cout << "No prime number missing!";
+
+    return (0 ^ 0);
+
+}
+
+// study smart, not hard
