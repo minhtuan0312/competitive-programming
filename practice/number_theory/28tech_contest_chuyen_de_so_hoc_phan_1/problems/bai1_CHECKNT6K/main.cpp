c47a7f0450f36bc938b1ffd7505826497cde53de
minhtuan0312 <70638910+minhtuan0312@users.noreply.github.com>
2025-11-02 13:49:07 +0700

    https://oj.28tech.com.vn/contest/nhomhcsinh_sohoc_2025/ranking/

---
+++ practice/number_theory/28tech_contest_chuyen_de_so_hoc_phan_1/problems/bai1_CHECKNT6K/main.cpp
@@ -0,0 +1,110 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define all(x) x.begin(), x.end()
+#define FOR(i, a, n) for (int i = a; i < n; i++)
+#define FORd(i, a, n) for (int i = n - 1; i >= a; i--)
+#define minhtuan0312 ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define sz(x) size(x)
+#define pb push_back
+#define fi first
+#define se second
+
+const ll mod = 1e9 + 7;
+
+template<class T>
+void maximize(T &x, const T &y) {
+    if (x < y) x = y;
+}
+
+template<class T>
+void minimize(T &x, const T &y) {
+    if (x > y) x = y;
+}
+
+template<typename T1, typename T2>
+void __print(const pair<T1, T2> &p);
+
+template<typename T, typename... V>
+void __print(const vector<T, V...> &v);
+
+template<typename T1, typename T2, typename... V>
+void __print(const map<T1, T2, V...> &m);
+
+template<typename T>
+void __print(const T &x) {cerr << x;}
+
+template<typename T1, typename T2>
+void __print(const pair<T1, T2> &p) {
+    cerr << '(';
+    __print(p.fi);
+    cerr << ", ";
+    __print(p.se);
+    cerr << ')';
+}
+
+template<typename T, typename... V>
+void __print(const vector<T, V...> &v) {
+    cerr << '[';
+    FOR(i, 0, sz(v)) {
+        if(i) cerr << ", ";
+        __print(v[i]);
+    }
+    cerr << ']';
+}
+
+template<typename T1, typename T2, typename... V>
+void __print(const map<T1, T2, V...> &m) {
+    cerr << '{';
+    bool first = 1;
+    for(const auto &x: m) {
+        if(!first) cerr << ", ";
+        first = 0;
+        __print(x);
+    }
+    cerr << '}';
+}
+
+template<typename T>
+void _print(T t) { __print(t);}
+void _print() {}
+template<typename T, typename... V>
+void _print(T t, V... v) {__print(t); if(sizeof...(v)) cerr << ", "; _print(v...); }
+
+#ifdef LOCAL
+#define deb(...) do {\
+    cerr << "[In " <<  __func__ << "(): line " << __LINE__ << "] [" << #__VA_ARGS__ << "] = [";\
+    _print(__VA_ARGS__);\
+    cerr << ']' << nl;\
+} while(0);
+#else
+#define deb(...)
+#endif
+
+bool isPrime(ll n) {
+    if(n == 2 || n == 3) return 1;
+    if(n < 5 || n % 2 == 0 || n % 3 == 0) return 0;
+    for(int p = 5; p <= sqrt(n); p += 6) {
+        if(n % p == 0 || n % (p + 2) == 0) return 0;
+    }
+    return 1;
+}
+
+int main(void) {
+    minhtuan0312;
+
+    #define TASK ""
+    if (fopen(TASK ".inp", "r")) {
+        freopen(TASK ".inp", "r", stdin);
+        freopen(TASK ".out", "w", stdout);
+    }
+
+    ll n; cin >> n;
+    cout << (isPrime(n)? "YES": "NO");
+
+}
+
+// study smart, not hard
