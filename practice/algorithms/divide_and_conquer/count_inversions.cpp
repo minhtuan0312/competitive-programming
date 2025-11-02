3fef9016e364a37e28fae4e49ea42022b5b665b4
minhtuan0312 <70638910+minhtuan0312@users.noreply.github.com>
2025-11-02 15:55:06 +0700

    update

---
+++ practice/algorithms/divide_and_conquer/count_inversions.cpp
@@ -0,0 +1,133 @@
+//https://oj.vnoi.info/problem/NKINV
+
+#include <bits/stdc++.h>
+#include <unordered_set>
+
+using namespace std;
+#define ll long long
+#define nl '\n'
+#define all(x) x.begin(), x.end()
+#define FOR(i, a, n) for (int i = a; i < n; i++)
+#define FORd(i, a, n) for (int i = n - 1; i >= a; i--)
+#define minhtuan0312 ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define sz(x) size(x)
+#define pb push_back
+#define fi first
+#define se second
+
+const ll mod = 1e9 + 7;
+
+template<class T>
+void maximize(T &x, const T &y) {
+    if (x < y) x = y;
+}
+
+template<class T>
+void minimize(T &x, const T &y) {
+    if (x > y) x = y;
+}
+
+template<typename T1, typename T2>
+void __print(const pair<T1, T2> &p);
+
+template<typename T, typename... V>
+void __print(const vector<T, V...> &v);
+
+template<typename T1, typename T2, typename... V>
+void __print(const map<T1, T2, V...> &m);
+
+template<typename T>
+void __print(const T &x) {cerr << x;}
+
+template<typename T1, typename T2>
+void __print(const pair<T1, T2> &p) {
+    cerr << '(';
+    __print(p.fi);
+    cerr << ", ";
+    __print(p.se);
+    cerr << ')';
+}
+
+template<typename T, typename... V>
+void __print(const vector<T, V...> &v) {
+    cerr << '[';
+    FOR(i, 0, sz(v)) {
+        if(i) cerr << ", ";
+        __print(v[i]);
+    }
+    cerr << ']';
+}
+
+template<typename T1, typename T2, typename... V>
+void __print(const map<T1, T2, V...> &m) {
+    cerr << '{';
+    bool first = 1;
+    for(const auto &x: m) {
+        if(!first) cerr << ", ";
+        first = 0;
+        __print(x);
+    }
+    cerr << '}';
+}
+
+template<typename T>
+void _print(T t) { __print(t);}
+void _print() {}
+template<typename T, typename... V>
+void _print(T t, V... v) {__print(t); if(sizeof...(v)) cerr << ", "; _print(v...); }
+
+#ifdef LOCAL
+#define deb(...) do {\
+    cerr << "[In " <<  __func__ << "(): line " << __LINE__ << "] [" << #__VA_ARGS__ << "] = [";\
+    _print(__VA_ARGS__);\
+    cerr << ']' << nl;\
+} while(0);
+#else
+#define deb(...)
+#endif
+
+const int limN = 1e6 + 5;
+int A[limN];
+
+ll cnt = 0; // đếm cặp nghịch thế
+void dnc(int l, int r) {
+    if(l >= r) return;
+    int mid = (l + r) >> 1;
+    dnc(l, mid);
+    dnc(mid + 1, r);
+
+    // merge
+    vector<int> tmp;
+    int i = l, j = mid + 1;
+    while(i <= mid && j <= r) {
+        if(A[i] <= A[j]) tmp.pb(A[i++]);
+        else {
+            cnt += mid - i + 1;
+            tmp.pb(A[j++]);
+        }
+    }
+    while(i <= mid) tmp.pb(A[i++]);
+    while(j <= r) tmp.pb(A[j++]);
+    FOR(k, l, r + 1) A[k] = tmp[k - l];
+}
+
+int main(void) {
+    minhtuan0312;
+
+    #define TASK ""
+    if (fopen(TASK ".inp", "r")) {
+        freopen(TASK ".inp", "r", stdin);
+        freopen(TASK ".out", "w", stdout);
+    }
+
+    int n; cin >> n;
+    FOR(i, 0, n) cin >> A[i];
+    dnc(0, n - 1);
+
+    cout << cnt;
+
+    return (0 ^ 0);
+
+}
+
+// study smart, not hard
