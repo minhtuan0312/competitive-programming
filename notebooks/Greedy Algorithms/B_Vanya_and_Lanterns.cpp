895a8ce3b8244b550e2d80c5a7984f21e0750b74
minhtuan0312 <70638910+minhtuan0312@users.noreply.github.com>
2026-06-16 10:20:43 +0700

    upd

---
+++ notebooks/Greedy Algorithms/B_Vanya_and_Lanterns.cpp
@@ -0,0 +1,167 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+
+using namespace std;
+#define bit(mask, i) ((mask >> i) & 1)
+#define ll long long
+#define nl '\n'
+#define all(x) x.begin(), x.end()
+#define FOR(i, a, n) for (int i = a; i < n; i++)
+#define FORd(i, a, n) for (int i = n - 1; i >= a; i--)
+#define minhtuan0312 ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+
+const ll mod = 1e9 + 7;
+
+template<class T>
+void maximize(T &x, const T &y) {
+    if (x < y) x = y;
+}
+
+template<class T>
+void minimize(T &x, const T &y) {
+    if (x > y) x = y;
+}
+
+template<typename T1, typename T2>
+void __print(const pair<T1, T2> &p);
+
+template<typename T, typename... V>
+void __print(const vector<T, V...> &v);
+
+template<typename T1, typename T2, typename... V>
+void __print(const map<T1, T2, V...> &m);
+
+template<typename T>
+void __print(const T &x) {cerr << x;}
+
+template<typename T1, typename T2>
+void __print(const pair<T1, T2> &p) {
+    cerr << '(';
+    __print(p.fi);
+    cerr << ", ";
+    __print(p.se);
+    cerr << ')';
+}
+
+template<typename T, typename... V>
+void __print(const vector<T, V...> &v) {
+    cerr << '[';
+    FOR(i, 0, sz(v)) {
+        if(i) cerr << ", ";
+        __print(v[i]);
+    }
+    cerr << ']';
+}
+
+template<typename T1, typename T2, typename... V>
+void __print(const map<T1, T2, V...> &m) {
+    cerr << '{';
+    bool first = 1;
+    for(const auto &x: m) {
+        if(!first) cerr << ", ";
+        first = 0;
+        __print(x);
+    }
+    cerr << '}';
+}
+
+template<typename T>
+void _print(T t) { __print(t);}
+void _print() {}
+template<typename T, typename... V>
+void _print(T t, V... v) {__print(t); if(sizeof...(v)) cerr << ", "; _print(v...); }
+
+#ifdef LOCAL
+#define deb(...) do {\
+    cerr << "[In " <<  __func__ << "(): line " << __LINE__ << "] [" << #__VA_ARGS__ << "] = [";\
+    _print(__VA_ARGS__);\
+    cerr << ']' << nl;\
+} while(0);
+#else
+#define deb(...)
+#endif
+
+/* ----------------------------- END OF TEMPLATE ---------------------------- */
+
+namespace greedy {
+
+    void solve() {
+
+        int n; cin >> n;
+        double l; cin >> l;
+        double A[n + 1];
+        FOR(i, 1 ,n + 1) {
+            cin >> A[i];
+        }
+        sort(A + 1, A + 1 + n);
+        double maxi = 0;
+        FOR(i, 1, n) {
+            maximize(maxi, abs(A[i + 1] - A[i]));
+        }
+        double res = maxi / 2;
+        maximize(res, A[1] - 0.0);
+        maximize(res, l - A[n]);
+        cout << fixed << setprecision(10) << res;
+
+    }
+
+}
+
+namespace binary_search {
+
+    void solve() {
+
+        int n; cin >> n;
+        double l; cin >> l;
+        double A[n + 1];
+        FOR(i, 1 ,n + 1) {
+            cin >> A[i];
+        }
+        sort(A + 1, A + 1 + n);
+
+        auto check = [&] (double k) -> bool {
+            if(A[1] - 0 > k) return 0;
+            if(l - A[n] > k) return 0;
+            FOR(i, 1, n) {
+                if(abs(A[i + 1] - A[i]) > 2 * k) return 0;
+            }
+            return 1;
+        };
+
+        double le = 0, ri = 1e9, res;
+        FOR(step, 1, 1000) {
+            double mi = (le + ri) / 2;
+            if(check(mi)) {
+                res = mi;
+                ri = mi;
+            } else {
+                le = mi;
+            }
+        }
+        cout << fixed << setprecision(10) << res;
+
+    }
+
+}
+
+int main(void) {
+    minhtuan0312;
+
+    #define TASK ""
+    if (fopen(TASK ".inp", "r")) {
+        freopen(TASK ".inp", "r", stdin);
+        freopen(TASK ".out", "w", stdout);
+    }
+    
+    binary_search::solve();
+    
+    return (0 ^ 0);
+
+}
+
+/* ------------------------------ THOU ART FAIR ----------------------------- */
