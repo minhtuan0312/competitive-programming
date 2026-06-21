cb9943016a0460d7b30c51db5856038048aa111b
minhtuan0312 <70638910+minhtuan0312@users.noreply.github.com>
2026-06-21 09:13:28 +0700

    upd

---
+++ notebooks/Greedy Algorithms/A_I_Wanna_Be_the_Guy.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+#include <unordered_set>
+
+using namespace std;
+#define bit(mask, i) ((mask >> i) & 1)
+#define ll long long
+#define nl '\n'
+#define all(x) x.begin(), x.end()
+#define FOR(i, a, n) for (int i = a; i < n; i++)
+#define FORd(i, a, n) for (int i = n - 1; i >= a; i--)
+#define minhtuan0312 ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0)
+#define sz(x) ((int)(x).size())
+#define pb push_back
+#define eb emplace_back
+#define fi first
+#define se second
+
+const ll mod = 1e9 + 7;
+
+template<class T>
+void maximize(T &x, const T &y) {
+    if (x < y) x = y;
+}
+
+template<class T>
+void minimize(T &x, const T &y) {
+    if (x > y) x = y;
+}
+
+template<typename T1, typename T2>
+void __print(const pair<T1, T2> &p);
+
+template<typename T, typename... V>
+void __print(const vector<T, V...> &v);
+
+template<typename T1, typename T2, typename... V>
+void __print(const map<T1, T2, V...> &m);
+
+template<typename T>
+void __print(const T &x) {cerr << x;}
+
+template<typename T1, typename T2>
+void __print(const pair<T1, T2> &p) {
+    cerr << '(';
+    __print(p.fi);
+    cerr << ", ";
+    __print(p.se);
+    cerr << ')';
+}
+
+template<typename T, typename... V>
+void __print(const vector<T, V...> &v) {
+    cerr << '[';
+    FOR(i, 0, sz(v)) {
+        if(i) cerr << ", ";
+        __print(v[i]);
+    }
+    cerr << ']';
+}
+
+template<typename T1, typename T2, typename... V>
+void __print(const map<T1, T2, V...> &m) {
+    cerr << '{';
+    bool first = 1;
+    for(const auto &x: m) {
+        if(!first) cerr << ", ";
+        first = 0;
+        __print(x);
+    }
+    cerr << '}';
+}
+
+template<typename T>
+void _print(T t) { __print(t);}
+void _print() {}
+template<typename T, typename... V>
+void _print(T t, V... v) {__print(t); if(sizeof...(v)) cerr << ", "; _print(v...); }
+
+#ifdef LOCAL
+#define deb(...) do {\
+    cerr << "[In " <<  __func__ << "(): line " << __LINE__ << "] [" << #__VA_ARGS__ << "] = [";\
+    _print(__VA_ARGS__);\
+    cerr << ']' << nl;\
+} while(0);
+#else
+#define deb(...)
+#endif
+
+/* ----------------------------- END OF TEMPLATE ---------------------------- */
+
+int main(void) {
+    minhtuan0312;
+
+    #define TASK ""
+    if (fopen(TASK ".inp", "r")) {
+        freopen(TASK ".inp", "r", stdin);
+        freopen(TASK ".out", "w", stdout);
+    }
+    
+    int n; cin >> n;
+    int p; cin >> p;
+    int A[p + 1];
+    FOR(i, 1, p + 1) cin >> A[i];
+    int q; cin >> q;
+    int B[q + 1];
+    FOR(i, 1, q + 1) cin >> B[i];
+    vector<int> comb;
+    FOR(i, 1, p + 1) comb.pb(A[i]);
+    FOR(i, 1, q + 1) comb.pb(B[i]);
+    sort(all(comb));
+    int idx = 0;
+    for(const int &x: comb) {
+        if(x == idx + 1) idx++;
+    }
+    cout << ((idx == n)? "I become the guy.": "Oh, my keyboard!");
+    
+    return (0 ^ 0);
+
+}
+
+/* ------------------------------ THOU ART FAIR ----------------------------- */
